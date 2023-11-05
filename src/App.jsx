a77902d86e78b27cc57bf4c375defc16a8b3e94f
BORRACH <eduardorodriguesp254@gmail.ccom>
2023-11-04 22:17:56 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "@chakra-ui/react";
+import Days from "./func/calculate";
+
+function App() {
+  return (
+    <Container>
+      <Days />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/func/calculate.jsx
@@ -0,0 +1,35 @@
+import { Box, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+const Days = function () {
+  const [daysRemaining, setDaysRemaining] = useState(null);
+
+  useEffect(() => {
+    // Obtém a data atual
+    const currentDate = new Date();
+
+    // Define a data de 01/10/2029
+    const targetDate = new Date("2029-10-01");
+
+    // Calcula a diferença em milissegundos
+    const differenceInMilliseconds = targetDate - currentDate;
+
+    // Converte a diferença para dias
+    const daysDifference = Math.floor(
+      differenceInMilliseconds / (1000 * 60 * 60 * 24)
+    );
+
+    // Define o número de dias restantes no estado
+    setDaysRemaining(daysDifference);
+  }, []);
+
+  return (
+    <Box className="App">
+      {daysRemaining !== null && (
+        <Text>Faltam {daysRemaining} dias até 01/10/2029.</Text>
+      )}
+    </Box>
+  );
+};
+
+export default Days;
