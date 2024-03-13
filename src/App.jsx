6b107bb10b069dda788a90797bca591ea9e107b0
BORRACH <eduardorodriguesp254@gmail.ccom>
2024-03-13 14:31:44 -0300

    adição do progresso de dias do ano em porcentagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Container, Flex, Grid, Stack, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, Grid, Stack, Text } from "@chakra-ui/react";
 import Days from "./components/calculate/Days";
 import Time from "./components/calculate/Time";
 import Months from "./components/calculate/Months";
 import Cards from "./components/Cards";
 import { AnimatePresence, motion } from "framer-motion";
 import SearchBar from "./components/SearchBar";
+import Percent from "./components/actualyYear/percent";
 
 function App() {
   return (
@@ -23,7 +24,9 @@ function App() {
             <Months />
             <Time />
           </Flex>
-          <Cards />
+          <Box>
+            <Percent />
+          </Box>
           <SearchBar />
         </Stack>
       </Container>
